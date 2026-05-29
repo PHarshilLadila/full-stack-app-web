972063e89accf2af54af0078403131a71fc385cd
Harshil Gajipara <harshilgajipara.ladila@gmail.com>
2026-05-29 16:39:02 +0530

    New Version 1.0.04

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,12 +3,12 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "1fbed79f5a3ce3a5a8a4ac69966352d7",
+const RESOURCES = {"flutter_bootstrap.js": "e950bb21c5672e16f958f54dad3ffafd",
 "version.json": "00f137b133e97355dc53c5a9ff510f57",
 "index.html": "542d64b9bc5b83980b24abc3fefc7c78",
 "/": "542d64b9bc5b83980b24abc3fefc7c78",
 "firebase-messaging-sw.js": "4601b1a3e4b8e6852baa4a0bf6d1d149",
-"main.dart.js": "b8c10c9780469582991798e6bab3746d",
+"main.dart.js": "58dae8f5f1d7cb2e9378817406dc67d6",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
@@ -23,7 +23,7 @@ const RESOURCES = {"flutter_bootstrap.js": "1fbed79f5a3ce3a5a8a4ac69966352d7",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "5d2d01752e6ec134fe77d9ef4baf86b7",
 "assets/shaders/ink_sparkle.frag": "ecc85a2e95f5e9f53123dcaf8cb9b6ce",
 "assets/AssetManifest.bin": "41c51749d73a0464cd3c276420be5ea3",
-"assets/fonts/MaterialIcons-Regular.otf": "d42866d1833315014acf58333b2b6b63",
+"assets/fonts/MaterialIcons-Regular.otf": "4388baeca6670111394b5580823bdbb2",
 "assets/assets/images/preview/home_screen.jpg": "3e94c8981232e10945e514d647548ace",
 "assets/assets/images/preview/splash_screen.jpg": "910143efcb491e92345632f186016eff",
 "assets/assets/images/preview/signup_screen.jpg": "c95df617c115b3712f4350bcd6fd781c",
